71d2af0d03888861ac9ccfc1356d0ad48ae8434f
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:32:02 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "iothub_endpoint_cosmosdb_accounts" {
+  description = "All iothub_endpoint_cosmosdb_account resources"
+  value       = azurerm_iothub_endpoint_cosmosdb_account.iothub_endpoint_cosmosdb_accounts
+}
+
